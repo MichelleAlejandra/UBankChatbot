8ff1963d6d78c9c12e79fef2d0b6048e2429ea64
Michelle Alejandra Gonzalez <michelle.gonzalez@celuweb.com>
2023-03-25 09:10:41 -0500

    Interfaz lista

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
+from tkinter import messagebox
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main(message):
+    if message != '' and message != ' ':
+        # Entrada
+        chatLog.insert(END,"_"*48 + "\n\n    ")
+        chatLog.image_create(END, image=imageUser)
+        chatLog.insert(END, "  " + message + "\n")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        # Salida
+        chatLog.insert(END, "_" * 48 + "\n\n    ")
+        chatLog.image_create(END, image=imageChat)
+        chatLog.insert(END, "  " + message + '\n')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        texto.set("")
+    else:
+        messagebox.showerror("Error", "Por favor ingrese un texto valido")
+
+# Interfaz grafica
+#Inicialización de la ventana principal del chatBot
+root = Tk()
+root.geometry('400x600')
+root.resizable(width=False, height=False)
+root.configure(background="white")
+lstChats = Listbox(root)
+
+logoPic = PhotoImage(file='./images/mensaje.png')
+imageChat = PhotoImage(file='./images/LogoUBank.png')
+imageUser = PhotoImage(file='./images/Usuario.png')
+send = PhotoImage(file='./images/send.png')
+txt_image = PhotoImage(file='./images/textfield.png')
+
+chatLog = Text(root, bd=0, bg="black", height="28.5", width="50", pady=10, )
+scrollbar = Scrollbar(root, command=chatLog.yview, cursor="heart")
+chatLog['yscrollcommand'] = scrollbar.set
+chatLog.config(foreground="white", font=("Verdana", 11))
+chatLog.place(x=0, y=0)
+
+# Inicia el chat
+chatLog.insert(END,  "\n" + "_"*48)
+chatLog.insert(END, "\n\n    ")
+chatLog.image_create(END, image=imageChat)
+chatLog.insert(END, "   Hola, ¿como puedo ayudarte?\n")
+
+
+Label(root, text="- UBank -", font=('Opensans', 14), bg="black", foreground="white").place(x=160, y=6)
+
+# TEXTFIELD
+texto = StringVar()
+eImage = Label(root,image=txt_image, border= 0, bg="white").place(x=32, y=553)
+txt = Entry(root, width=34, border=0, font=('bold', 11), bg="#EEEEEE", textvariable=texto).place(x=42, y=560)
+
+# BOTÓN ENVIAR
+Button(root, image=send, command=lambda:main(texto.get()), background="white", borderwidth=0).place(x=335, y=556)
+
+root.title("Chatbot Consultor de Mascotas")
+root.iconphoto(False, logoPic)
+root.mainloop()
