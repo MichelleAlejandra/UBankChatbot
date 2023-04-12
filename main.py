8324d281c3107273b410bf4e4a7795dabdd43284
Michelle Alejandra <michellea.gonzalezh@uqvirtual.edu.co>
2023-04-11 19:37:18 -0500

    Correcciones

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 # -------------------------------------- Librerías
-
 from tkinter import *
 from tkinter import messagebox
+
 import json
-import pickle
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.stem.lancaster import LancasterStemmer #Transformar palabras - quitar letras demas
-stemmer = LancasterStemmer()
-import numpy as np
-import tensorflow as tf
-import tflearn
 import random
 import re
 from collections import Counter
 
+import numpy as np
+import pickle
+import nltk
+import tensorflow as tf
+import tflearn
+from nltk.tokenize import word_tokenize
+from nltk.stem.lancaster import LancasterStemmer  # Transformar palabras - quitar letras de mas
+
+stemmer = LancasterStemmer()
+
 # Inicialización de variables
-u_quest = []
-u_ans = []
-quetions_with_user = []
+questions_asked = []
+answers_given = []
 preguntas_usuario = []
 users = []
 words = []
@@ -26,27 +27,26 @@ tags = []
 docs_x = []
 docs_y = []
 
-
 # --------------------------------------- Entrenamiento chat
-with open('./preguntas-respuesta.json', encoding='utf-8') as file:
+with open('content/preguntas-respuesta.json', encoding='utf-8') as file:
     data = json.load(file)
 
 for content in data['contenido']:
     for pattern in content['posibles-preguntas']:
         # Tokeniza cada palabra en la frase
-        wrds_list  = nltk.word_tokenize(pattern) 
+        wrds_list = nltk.word_tokenize(pattern)
         # Agrega las palabras tokenizadas a la lista de palabras
         words.extend(wrds_list)
         # Agrega la frase tokenizada a la lista de documentos
         docs_x.append(wrds_list)
         # Agrega el tag de la intención a la lista de etiquetas
-        docs_y.append(content['tag']) 
+        docs_y.append(content['tag'])
 
-        if content['tag'] not in tags: #Se almacenan todos los tags mientras no esten dentro
+        if content['tag'] not in tags:  # Se almacenan todos los tags mientras no esten dentro
             tags.append(content['tag'])
 
 # Eliminar duplicados y ordenar la lista de palabras
-words = [stemmer.stem(w.lower()) for w in words if w!='?'] #Casteo de palabra
+words = [stemmer.stem(w.lower()) for w in words if w != '?']  # Casteo de palabra
 words = sorted(list(set(words)))
 tags = sorted(tags)
 
@@ -67,11 +67,11 @@ for x, doc in enumerate(docs_x):
             bag.append(0)
 
     # Crear un array de salida con un 1 en la posición correspondiente a la etiqueta del documento
-    output_row = out_empty[:] 
+    output_row = out_empty[:]
     # Establece la posición correspondiente a la etiqueta en 1
     output_row[tags.index(docs_y[x])] = 1
-    
-    #Añadir los arrays de entrada y salida a los arrays de entrenamiento y salida
+
+    # Añadir los arrays de entrada y salida a los arrays de entrenamiento y salida
     training.append(bag)
     output.append(output_row)
 
@@ -80,38 +80,39 @@ training = np.array(training)
 output = np.array(output)
 
 # Guarda las palabras, etiquetas y entrenamiento en un archivo
-with open('data.pickle','wb') as fileP:
-    pickle.dump((words, tags, training, output), fileP)
+with open('files/data.pickle', 'wb') as filePicker:
+    pickle.dump((words, tags, training, output), filePicker)
 
 # Carga las palabras, etiquetas y entrenamiento desde el archivo
-with open('data.pickle', 'rb') as fileP:
-    words, tags, training, output = pickle.load(fileP)
+with open('files/data.pickle', 'rb') as filePicker:
+    words, tags, training, output = pickle.load(filePicker)
 
 tf.compat.v1.reset_default_graph()
-net = tflearn.input_data(shape=[None,len(training[0])])
-net = tflearn.fully_connected(net,10)
-net = tflearn.fully_connected(net,10)
-net = tflearn.fully_connected(net,len(output[0]),activation='softmax') # Predicciones
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, len(output[0]), activation='softmax')  # Predicciones
 net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-model.fit(training, output, n_epoch= 1000, batch_size=10, show_metric=True) #Va a ver la información 1000 veces y cuantas entradas
+model.fit(training, output, n_epoch=1000, batch_size=10,
+          show_metric=True)  # Va a ver la información 1000 veces y cuantas entradas
 model.save('./files/modelo.tflearn')
+
 
 # Función para clasificar una entrada y obtener la respuesta del chatbot
 def classify(sentence):
-   
     bag = [0 for _ in range(len(words))]
     input_processed = nltk.word_tokenize(sentence)
     input_processed = [stemmer.stem(word.lower()) for word in input_processed]
-    
+
     for individual_word in input_processed:
         for i, word in enumerate(words):
 
             if word == individual_word:
                 bag[i] = 1
-    
+
     results = model.predict([np.array(bag)])
     index_results = np.argmax(results)
     tag = tags[index_results]
@@ -121,87 +122,85 @@ def classify(sentence):
             answers = tagA['posibles-respuesta']
             return random.choice(answers)
 
+
 # ------------------------------------------------------- Entrenamiento
-with open('base-datos.json', 'r') as archivo:
-    datos = json.load(archivo)
+with open('content/base-datos.json', 'r', encoding='utf-8') as archivo:
+    collected_data = json.load(archivo)
+
 
 # Se escribe el archivo JSON con todas las preguntas
 def escribirJson():
-    data = []
     for user in users:
-        quetions_with_user.append(datos)
-        quetions_with_user.append({
-            'user': user,   
-            'pregunta' : u_quest, 
-            'respuesta' : u_ans, 
+        collected_data["data"].append({
+            'user': user,
+            'pregunta': questions_asked,
+            'respuesta': [texto.strip().replace('\n            ', '') for texto in answers_given],
         })
 
-    with open('base-datos.json', 'w') as archivo:
-        json.dump(quetions_with_user,archivo,indent=4, ensure_ascii=False)
+    with open('content/base-datos.json', 'w', encoding='utf-8') as archivo:
+        json.dump(collected_data, archivo, indent=4, ensure_ascii=False)
 
 
 # Se obtienen todas las preguntas registradas en el archivo JSON
-for user in datos["data"]:
+for user in collected_data["data"]:
     for pregunta in user["pregunta"]:
         preguntas_usuario.append(pregunta)
 
-def explorarDatos():
 
+def explorarDatos():
     total_preguntas = len(preguntas_usuario)
     print('El total de preguntas de la exploración son:', total_preguntas, "\n")
 
     preguntas_frecuentes = Counter(preguntas_usuario).most_common(10)
     print('Las preguntas más frecuentes son: ', preguntas_frecuentes, "\n")
 
-    pregunta_aleatoria = random.choice(preguntas_usuario)
-    print('Una pregunta aleatoria de las que nos han proporcionado los usuarios es: ',pregunta_aleatoria, "\n")
-    
-    preguntas_procesadas = [[LancasterStemmer().stem(palabra) for palabra in word_tokenize(pregunta.lower())] for pregunta in preguntas_usuario]
+    if len(preguntas_usuario) > 0:
+        pregunta_aleatoria = random.choice(preguntas_usuario)
+        print('Una pregunta aleatoria de las que nos han proporcionado los usuarios es: ', pregunta_aleatoria, "\n")
+
+    preguntas_procesadas = [[LancasterStemmer().stem(palabra) for palabra in word_tokenize(pregunta.lower())] for
+                            pregunta in preguntas_usuario]
     palabras_preguntas = [palabra for pregunta in preguntas_procesadas for palabra in pregunta]
     palabras_frecuentes_preguntas = Counter(palabras_preguntas).most_common(10)
-    print('Las palabras que se utilizan en las preguntas con su frecuencia son: ' ,palabras_frecuentes_preguntas, "\n")
-
+    print('Las palabras que se utilizan en las preguntas con su frecuencia son: ', palabras_frecuentes_preguntas, "\n")
 
 
 # ------------------------------------------------------- Función principal
 def main(message):
-    
     explorarDatos()
-
     if message != '' and message != ' ':
-        
         patron = r"mi nombre es (\w+)"
         result = re.search(patron, message)
         if result:
             users.append(result.group(1))
 
         # Entrada
-        chatLog.insert(END,"_"*48 + "\n\n    ")
+        chatLog.insert(END, "_" * 38 + "\n\n    ")
         chatLog.image_create(END, image=imageUser)
         chatLog.insert(END, "  " + message + "\n")
 
-        answer = classify(message)
+        respuestaChat = classify(message)
 
         # Salida
-        chatLog.insert(END, "_" * 48 + "\n\n    ")
+        chatLog.insert(END, "_" * 38 + "\n\n    ")
         chatLog.image_create(END, image=imageChat)
-        chatLog.insert(END, "  " + answer + '\n')
+        chatLog.insert(END, "  " + respuestaChat + '\n')
 
         texto.set("")
 
-        u_quest.append(message)
-        u_ans.append(answer)
+        questions_asked.append(message)
+        answers_given.append(respuestaChat)
 
-        if message == 'Adios' or message == 'Hasta la proxima' or message == 'Chao' or message == 'Hasta luego' or message == 'Hasta pronto' or message == 'Nos vemos' or message == 'Gracias' :
+        if message == 'Adios' or message == 'Hasta la proxima' or message == 'Chao' or message == 'Hasta luego' or message == 'Hasta pronto' or message == 'Nos vemos' or message == 'Gracias':
             escribirJson()
-            
             root.destroy()
 
     else:
         messagebox.showerror("Error", "Por favor ingrese un texto valido")
 
-#-------------------------------------------------------------- Interfaz grafica
-#Inicialización de la ventana principal del chatBot
+
+# Interfaz grafica
+# Inicialización de la ventana principal del chatBot
 root = Tk()
 root.geometry('400x600')
 root.resizable(width=False, height=False)
@@ -214,32 +213,28 @@ imageUser = PhotoImage(file='./images/Usuario.png')
 send = PhotoImage(file='./images/send.png')
 txt_image = PhotoImage(file='./images/textfield.png')
 
-chatLog = Text(root, bd=0, bg="black", height="28.5", width="50", pady=10, )
+chatLog = Text(root, bd=0, bg="black", height="28.5", width="38", pady=10, padx=10)
 scrollbar = Scrollbar(root, command=chatLog.yview, cursor="heart")
 chatLog['yscrollcommand'] = scrollbar.set
 chatLog.config(foreground="white", font=("Verdana", 11))
 chatLog.place(x=0, y=0)
 
 # Inicia el chat
-chatLog.insert(END,  "\n" + "_"*48)
+chatLog.insert(END, "\n" + "_" * 38)
 chatLog.insert(END, "\n\n    ")
 chatLog.image_create(END, image=imageChat)
 chatLog.insert(END, "   Hola, ¿cómo es tu nombre?\n")
-
 
 Label(root, text="- UBank -", font=('Opensans', 14), bg="black", foreground="white").place(x=160, y=6)
 
 # TEXTFIELD
 texto = StringVar()
-eImage = Label(root,image=txt_image, border= 0, bg="white").place(x=32, y=553)
+eImage = Label(root, image=txt_image, border=0, bg="white").place(x=32, y=553)
 txt = Entry(root, width=34, border=0, font=('bold', 11), bg="#EEEEEE", textvariable=texto).place(x=42, y=560)
 
 # BOTÓN ENVIAR
-Button(root, image=send, command=lambda:main(texto.get()), background="white", borderwidth=0).place(x=335, y=556)
+Button(root, image=send, command=lambda: main(texto.get()), background="white", borderwidth=0).place(x=335, y=556)
 
-root.title("Chatbot Consultor de Mascotas")
+root.title("UBank Chatbot")
 root.iconphoto(False, logoPic)
 root.mainloop()
-
-
-
